ca00ce2fb1a588ee7eda5dfccf59ff8c747609f6
Aimopotis <morpheustudio@gmail.com>
2018-04-01 10:10:26 +0300

    Keep on Sharing

---
+++ easyHttp2.js
@@ -0,0 +1,70 @@
+/**
+ * EasyHTTP Library Fetch
+ * @version 2.0.0
+ * @author Costas Zeimpekis
+ * @license MIT
+ * 
+ */
+
+class EaasyHTTP {
+  //Make HTTP GET Request
+    get(url){
+      return new Promise((resolve, reject)=>{
+      fetch(url)
+      .then(res=>res.json())
+      .then(data=> resolve(data))
+      .catch(err=> reject(err))
+      });
+    };
+
+
+    //Make an HTTP Post Request
+    post(url, data){
+      return new Promise((resolve, reject)=>{
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          "Content-type": "application/json"
+        },
+        body : JSON.stringify(data)
+      })
+      .then(res=>res.json())
+      .then(data=> resolve(data))
+      .catch(err=> reject(err))
+      });
+    }
+
+      //Make an HTTP Put Request
+      put(url, data){
+        return new Promise((resolve, reject)=>{
+        fetch(url, {
+          method: 'PUT',
+          headers: {
+            "Content-type": "application/json"
+          },
+          body : JSON.stringify(data)
+        })
+        .then(res=>res.json())
+        .then(data=> resolve(data))
+        .catch(err=> reject(err))
+        });
+      }
+
+      //Make an HTTP DELETE REquest
+      delete(url){
+        return new Promise((resolve, reject)=>{
+        fetch(url, {
+          method: 'DELETE',
+          headers: {
+            "Content-type": "application/json"
+          },
+        })
+        .then(res=>res.json())
+        .then(()=> resolve('Resource Deleted'))
+        .catch(err=> reject(err))
+        });
+      }
+  };
+
+  
+  
